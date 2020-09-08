e6d821580608e298df35f16d39ae1612068d6159
saidov017 <58263437+saidov017@users.noreply.github.com>
2020-09-08 12:32:57 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Ball ball = new Ball(2, 2);
+        System.out.println(ball);
+
+        ball.move(17, 3);
+        System.out.println(ball);
+    }
+}
