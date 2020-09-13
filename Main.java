f3275173d78c27f405b61bf95573d72dcc8dfabd
saidov017 <58263437+saidov017@users.noreply.github.com>
2020-09-13 19:37:40 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,10 +3,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        Ball ball = new Ball(2, 2);
+        Ball ball = new Ball(84, 76);
         System.out.println(ball);
 
-        ball.move(17, 3);
+        ball.move(38, 19);
         System.out.println(ball);
     }
 }
